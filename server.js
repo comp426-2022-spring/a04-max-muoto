b1694a364f88f2c8572856ca88c5317a64288732
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-03-21 14:09:43 -0400

    Small Changes

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ argv["help"]
 argv["debug"]
 
 
+const HTTP_PORT = argv.port || 5000;
+
 if (argv.help) {
   console.log("server.js [options]")
   console.log("--port	Set the port number for the server to listen on. Must be an integer between 1 and 65535.");
@@ -20,15 +22,17 @@ if (argv.help) {
   process.exit(0);
 }
 
-if (argv.debug == "true") {
+debug = false;
+log = true;
 
+if (argv.debug == "true") {
+  debug = true;
 }
 
 if (argv.log == "false") {
-
+  log = false;
 }
 
-const HTTP_PORT = argv.port || 5000;
 
 // Start an app server
 const server = app.listen(HTTP_PORT, () => {
@@ -36,40 +40,21 @@ const server = app.listen(HTTP_PORT, () => {
 });
 
 
+if (debug == true) {
+    app.get('/app/log/access', (req, res) => {
+      flip_result = coinFlip();
+      res.status(200).json({"flip" : flip_result});
+  });
+
+  app.get('/app/error', (req, res) => {
+    throw new Error('Error test successful.')
+  });
+}
 
 
-// Define Check Endpoints
-app.get('/app/', (req, res) => {
-    // Respond with status 200
-        res.statusCode = 200;
-    // Respond with status message "OK"
-        res.statusMessage = 'OK';
-        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-        res.end(res.statusCode+ ' ' +res.statusMessage)
-    });
+if (log == true) {
 
-
-app.get('/app/flip/', (req, res) => {
-    flip_result = coinFlip();
-    res.status(200).json({"flip" : flip_result});
-});
-
-app.get('/app/flips/:number', (req, res) => {
-    amt = req.params.number
-    coin_flips = coinFlips(amt)
-    flips_counted = countFlips(coin_flips)
-    res.status(200).json({"raw" : coin_flips, "summary" : flips_counted});
-
-});
-
-app.get('/app/flip/call/heads', (req, res) => {
-    res.status(200).json({"result" : flipACoin("heads")});
-});
-
-app.get('/app/flip/call/tails', (req, res) => {
-    res.status(200).json({"result" : flipACoin("tails")});
-});
-
+}
 
 
 // Default response for any other request
@@ -78,67 +63,7 @@ app.use(function(req, res){
 });
 
 
-function coinFlip() {
-    let rand = Math.floor(Math.random() * 2);
-    if (rand == 1) {
-      return "heads";
-    } else {
-      return "tails";
-    }
-  }
-  
-  
-  function coinFlips(flips) {
-    let result = [];
-    for (let i = 0; i < flips; i++) {
-      result.push(coinFlip());
-    }
-    return result;
-  }
-  
-  
-  function countFlips(array) {
-    let num_tails = 0;
-    let num_heads = 0;
-    for (let i = 0; i < array.length; i++) {
-      if (array[i] == "heads") {
-        num_heads += 1;
-      } else {
-        num_tails += 1;
-      }
-    }
-    if (num_heads == 0) {
-      return {
-        tails: num_tails
-      };
-    }
-    if (num_tails == 0) {
-      return {
-        heads: num_heads,
-      };
-    }
-    return {
-      heads: num_heads,
-      tails: num_tails
-    };
-  }
-  
-  
-  function flipACoin(call) {
-    let coin  = coinFlip();
-    if (coin == call) {
-      return {
-        call: call,
-        flip: coin,
-        result: "win"
-      };
-    } else {
-      return {
-        call: call,
-        flip: coin,
-        result: "lose"
-      };
-    }
-  }
-  
-  
+function create_db() {
+  const db = new Database('log.db');
+
+}
