7b1ce562655daf70ed606ff2816e158c1e9700a0
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-03-25 15:26:07 -0400

    Minor Addition

--- server.js
+++ server.js
@@ -67,4 +67,7 @@ function create_db() {
   const db = new Database('log.db');
   const stmt = db.prepare(`
   SELECT name FROM sqlite_master WHERE type='table' and name='userinfo';`);
+  let row = stmt.get();
+
+
 }
